af80050c6f83b461f699bfa6149f4a87e8073de3
bryanpow <ramosbusiness544@gmail.com>
2024-04-02 23:02:34 -0400

    server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const {logRoutes} = require('./middleware/loggingMiddleware');
+
+const app = express();
+
+app.use(logRoutes);
+const port = process.env.PORT || 8080;
+const pathToDist = path.join(__dirname, '../vite-project/dist');
+express.static(pathToDist);
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+})
